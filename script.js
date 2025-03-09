4c8ef811ffa5f6102d67c51986eac8e96e4f0432
SmartAff <smartaffiliatee@gmail.com>
2025-03-09 07:08:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    $('#book').turn({
+        width: 600,
+        height: 400,
+        autoCenter: true,
+        duration: 1000
+    });
+});
